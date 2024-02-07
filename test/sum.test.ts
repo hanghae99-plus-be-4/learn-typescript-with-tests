70e8e8936f369175794920d8e137c2f3f4f5e490
gwonyeong <qwe980121@naver.com>
2024-02-07 21:29:46 +0900

    feat: 테스트코드 작성

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,3 +1,15 @@
-test('sample', () => {
-    expect(1 + 1).toBe(2)
-})
+// test('sample', () => {
+//     expect(1 + 1).toBe(2)
+// })
+
+import { sum } from '../src/sume';
+
+// console.log는 테스트하기 어렵다
+test('sum all numbers in array', () => {
+    const arr = [1, 2, 3, 4, 5];
+
+    const got = sum(arr);
+    const want = 15;
+
+    expect(got).toBe(want);
+});
