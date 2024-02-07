f752b9232aeccfb98049df791820ba66ca40d517
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-02-07 21:29:33 +0900

    feat: greet function

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,12 @@
-export function sum(arr: number[]): number {
-    return arr.reduce((acc, val) => acc + val)
+// export function greet(message: string) {
+//     // how to test?
+//     console.log(message)
+// }
+
+export interface Stdout {
+    write(message: string): void
+}
+
+export function greet(stdout: Stdout, message: string) {
+    stdout.write(message)
 }
