d352c06a0cfde9fe93fdd851198891b3037ebfcd
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-02-07 21:24:09 +0900

    refactor(blue): sum function loop to reduce

--- src/sum.ts
+++ src/sum.ts
@@ -1,7 +1,3 @@
 export function sum(arr: number[]): number {
-    let r = 0
-    for (let i = 0; i < arr.length; i++) {
-        r += arr[i]
-    }
-    return r
+    return arr.reduce((acc, val) => acc + val)
 }
