1fd71fa3da4c607bad694db66cfc56e5d2d31cce
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-02-07 21:21:58 +0900

    test(red): sum function

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,3 +1,14 @@
+import {sum} from '../src/sum'
+
 test('sample', () => {
     expect(1 + 1).toBe(2)
+})
+
+test('sum all numbers of the array', () => {
+    const arr = [1, 2, 3, 4, 5]
+
+    const got = sum(arr)
+    const want = 15
+
+    expect(got).toBe(want)
 })
